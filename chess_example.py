341187199bf4d9ea25f518a26005c4d9d819707c
Jace Iverson <jaceiverson@Jaces-MacBook-Pro.local>
2021-05-13 21:40:57 -0400

    changed to accept 5 aggregations as well as an example plot

--- chess_example.py
+++ chess_example.py
@@ -2,4 +2,7 @@ from chess import ChessAPI
 
 c = ChessAPI('jaceiverson')
 c.archive_grab()
-df,d,r,m = c.opp('EldrickLover')
+df,d,r,m,ms = c.opp('EldrickLover')
+
+#plot results over time
+ms.unstack(level=2).plot(kind='bar',stacked=True,)
